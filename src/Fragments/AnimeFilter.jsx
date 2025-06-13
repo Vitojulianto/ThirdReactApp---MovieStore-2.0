84dade57310586c110d8c7ba32c1c7dd3f4f97d7
Vito Julianto <76040744+Vitojulianto@users.noreply.github.com>
2025-06-13 12:29:57 +0700

    Update AnimeFilter.jsx

--- src/Fragments/AnimeFilter.jsx
+++ src/Fragments/AnimeFilter.jsx
@@ -1,27 +1,30 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-function AnimeFilter({onFilter}) {
-    const [genres, setGenres] = useState([])
-    useEffect(() => {
+
+function AnimeFilter({ onFilter }) {
+  const [genres, setGenres] = useState([])
+
+  useEffect(() => {
     const fetchGenres = async () => {
-            const response = await axios.get('https://api.jikan.moe/v4/genres/anime')
-            setGenres(response.data.data)
-            }
-            fetchGenres()
-        },[])
-    
+      const response = await axios.get('https://api.jikan.moe/v4/genres/anime')
+      setGenres(response.data.data)
+    }
+    fetchGenres()
+  }, [])
+
   return (
-    <div>
-      <div className='p-2 text-black-500'>
-      <select onChange={(e) => onFilter(e.target.value)} className='border px-2 py-1 rounded'>
-        <option value="" className="text-slate-500">All Genres</option>
+    <div className="p-2">
+      <select
+        onChange={(e) => onFilter(e.target.value)}
+        className="border px-2 py-1 rounded text-black bg-white shadow-md focus:outline-none focus:ring-2 focus:ring-pink-400"
+      >
+        <option value="">All Genres</option>
         {genres.map((genre) => (
           <option key={genre.mal_id} value={genre.mal_id}>
             {genre.name}
           </option>
         ))}
       </select>
-    </div>
     </div>
   )
 }
